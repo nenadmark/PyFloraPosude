e345c2c8a6d9b866c176415b83dd991248826bee
nenadmark <45971785+nenadmark@users.noreply.github.com>
2023-04-18 12:45:46 +0200

    Update --insert_users.sql

--- --insert_users.sql
+++ --insert_users.sql
@@ -1,3 +1,3 @@
 -- SQLite
 
-INSERT INTO users (id, name, email, is_admin, password) VALUES ("01","admin", "ad", true, "12");
+INSERT INTO users (id, name, email, is_admin, password) VALUES ("01","admin", "admin", true, "0000");
